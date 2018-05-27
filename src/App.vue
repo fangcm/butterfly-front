1265ab739f2cd0d51c494871703c72c621bf9c84
fangcm <fangcm2008@qq.com>
2018-05-27 20:58:17 +0800

    2

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,31 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+      <vHeader/>
+      <router-view></router-view>
+      <div v-transfer-dom><loading :show="vLoadingIsShow" :text="`加载中...`"></loading></div>
+      <toast v-model="toastMsg.isShow" type="text" :width="toastMsg.width" :time="toastMsg.time" is-show-mask :position="`middle`">{{toastMsg.text}}</toast>
   </div>
 </template>
 
 <script>
+import {Loading,TransferDomDirective as TransferDom,Toast} from 'vux';
+import vHeader from '@/components/header/v-header'
+import vFooter from '@/components/footer/v-footer';
+import { mapState } from 'vuex';
 export default {
-  name: 'app'
+  name: 'app',
+  directives: {
+    TransferDom
+  },
+  components:{Loading, Toast, vHeader, vFooter},
+  data (){
+  	return {
+
+  	}
+  },
+  computed:{
+    ...mapState(['vLoadingIsShow','toastMsg'])
+    // ...mapState(['vLoadingIsShow'])
+  }
 }
 </script>
-
-<style lang="less">
-@import '~vux/src/styles/reset.less';
-
-body {
-  background-color: #fbf9fe;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,28 +1,27 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// import 'babel-ployfill'
 import Vue from 'vue'
-import FastClick from 'fastclick'
-import VueRouter from 'vue-router'
+import fastclick from 'fastclick'
 import App from './App'
-import Home from './components/HelloFromVux'
-
-Vue.use(VueRouter)
-
-const routes = [{
-  path: '/',
-  component: Home
-}]
-
-const router = new VueRouter({
-  routes
-})
-
-FastClick.attach(document.body)
-
+import router from './router'
+import 'font-awesome/css/font-awesome.css'
+import Vuex from 'vuex'
+// import infiniteScroll from 'vue-infinite-scroll'
+import store from './store'
+// import Vuex from 'vuex'
+// import vuexI18n from 'vuex-i18n'
+// Vue.use(infiniteScroll)
+// Vue.use(VueResource)
+Vue.use(Vuex)
+fastclick.attach(document.body)
 Vue.config.productionTip = false
+
 
 /* eslint-disable no-new */
 new Vue({
+  // el: '#app',
   router,
-  render: h => h(App)
-}).$mount('#app-box')
+  store,
+  render: h=>h(App)
+}).$mount('#app')
