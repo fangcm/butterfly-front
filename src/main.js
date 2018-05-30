abb2e63966e658b8e27c6bc7bb54feaa32528858
fangcm <fangcm@gmail.com>
2018-05-30 12:21:07 +0800

    vue init airyland/vux2 butterfly-front

--- src/main.js
+++ src/main.js
@@ -1,11 +1,28 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-import App from './App.vue'
+import FastClick from 'fastclick'
+import VueRouter from 'vue-router'
+import App from './App'
+import Home from './components/HelloFromVux'
 
-Vue.use(ElementUI)
+Vue.use(VueRouter)
 
-new Vue({
-  el: '#app',
-  render: h => h(App)
+const routes = [{
+  path: '/',
+  component: Home
+}]
+
+const router = new VueRouter({
+  routes
 })
+
+FastClick.attach(document.body)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app-box')
