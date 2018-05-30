29af1bc328a2c9f957af9e22e9a5b25e36a05365
fangcm <fangcm@gmail.com>
2018-05-30 10:55:29 +0800

    vue init webpack butterfly-front

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -3,13 +3,14 @@ const path = require('path')
 const utils = require('./utils')
 const config = require('../config')
 const vueLoaderConfig = require('./vue-loader.conf')
-const vuxLoader = require('vux-loader')
 
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
 }
 
-let webpackConfig = {
+
+
+module.exports = {
   context: path.resolve(__dirname, '../'),
   entry: {
     app: './src/main.js'
@@ -38,7 +39,7 @@ let webpackConfig = {
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        include: [resolve('src'), resolve('test')]
+        include: [resolve('src'), resolve('test'), resolve('node_modules/webpack-dev-server/client')]
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
@@ -65,29 +66,17 @@ let webpackConfig = {
         }
       }
     ]
+  },
+  node: {
+    // prevent webpack from injecting useless setImmediate polyfill because Vue
+    // source contains it (although only uses it if it's native).
+    setImmediate: false,
+    // prevent webpack from injecting mocks to Node native modules
+    // that does not make sense for the client
+    dgram: 'empty',
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty',
+    child_process: 'empty'
   }
 }
-
-
-module.exports = vuxLoader.merge(webpackConfig, {
-  plugins: [
-    'vux-ui',
-    'progress-bar',
-    {
-      name: 'duplicate-style',
-      options: {
-        cssProcessorOptions : {
-          safe: true,
-          zindex: false,
-          autoprefixer: {
-            add: true,
-            browsers: [
-              'iOS >= 7',
-              'Android >= 4.1'
-            ]
-          }
-        }
-      }
-    }
-  ]
-})
--- src/main.js
+++ src/main.js
@@ -1,23 +1,13 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-// import 'babel-ployfill'
 import Vue from 'vue'
-import fastclick from 'fastclick'
 import App from './App'
-import router from './router'
 
-// import Vuex from 'vuex'
-// import vuexI18n from 'vuex-i18n'
-
-// Vue.use(VueResource)
-// Vue.use(Vuex)
-fastclick.attach(document.body)
 Vue.config.productionTip = false
-
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
-  render: h=>h(App)
-}).$mount('#app')
+  components: { App },
+  template: '<App/>'
+})
