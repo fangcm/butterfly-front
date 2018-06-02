e444e8dba6411d9aad39d7ef67d86aa23c6f5ca1
fangcm <fangcm2008@qq.com>
2018-06-02 09:39:36 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,32 @@
 <template>
   <div id="app">
     <router-view></router-view>
+    <app-footer></app-footer>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app',
-  data () {
-    return {
-      ws: null,
-      transitionName: 'animated fadeIn'
-    }
-  },
-  watch: {
-    '$route' (to, from) {
-      this.transitionName = 'animated ' + (to.meta.index >= from.meta.index ? 'slideInRight' : 'slideInLeft')
-    }
-  },
-  created () {
-    if (window.plus) {
-      this.plusReady()
-    } else {
-      document.addEventListener('plusready', this.plusReady, false)
-    }
-  },
-  methods: {
-    plusReady () {
-      let self = this
-      window.plus.key.addEventListener('backbutton', function () {
-        self.goBack() && window.confirm('确认退出？') && window.plus.runtime.quit()
-      }, false)
-    },
-    /* 返回，若已在首页，则返回true，提示用户是否退出系统 */
-    goBack () {
-      if (this.$route.path !== '/') {
-        this.$router.go(-1)
-        return false
-      } else {
-        return true
-      }
+  import AppFooter from '@/components/common/Footer'
+
+  export default {
+    name: 'app',
+    components: {
+      AppFooter
     }
   }
-}
 </script>
 
 <style lang="less">
-@import '~vux/src/styles/reset.less';
-@import './style/variables.less';
-@import './style/common.less';
-html, body, #app {
-  height: 100%;
-}
-body {
-  background-color: @bg-color;
-  height: 100%;
-}
+  @import '~vux/src/styles/reset.less';
+  @import './assets/style/variables.less';
+  @import './assets/style/common.less';
+
+  html, body, #app {
+    height: 100%;
+  }
+
+  body {
+    background-color: @bg-color;
+    height: 100%;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,17 +2,13 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import FastClick from 'fastclick'
-import App from './App'
 import router from './router'
+import App from './App'
 import store from './store'
-import plugin from './apps/plugin'
-import 'animate.css'
 
 FastClick.attach(document.body)
-
 Vue.config.productionTip = false
 
-Vue.use(plugin)
 /* eslint-disable no-new */
 new Vue({
   store,
