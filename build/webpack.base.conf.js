1201381d99c6889d55e5d1c4351a93cd945b531e
fangcm <fangcm2008@qq.com>
2018-06-18 22:32:37 +0800

    webpack 初始配置

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -34,11 +34,8 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.vue', '.json'],
     alias: {
+      'vue$': 'vue/dist/vue.esm.js',
       '@': resolve('src'),
-      'api': resolve('src/api'),
-      'views': resolve('src/views'),
-      'components': resolve('src/components'),
-      'common': resolve('src/common')
     }
   },
   module: {
