6655e82c8548a89c60a6d45bb2b060050e4e418d
fangcm <fangcm@gmail.com>
2018-06-22 18:10:04 +0800

    o

--- src/components/DrawerBar.vue
+++ src/components/DrawerBar.vue
@@ -1,13 +1,15 @@
 <template>
   <div>
     <mu-drawer :open="open" :docked="false" @close="toggle()">
-      <mu-appbar title="菜单"></mu-appbar>
-      <mu-list slot="content">
+      <mu-appbar title="菜单" color="secondary"></mu-appbar>
+      <mu-list>
         <mu-list-item button>
           <mu-list-item-title>关闭</mu-list-item-title>
         </mu-list-item>
-        <mu-list-item-title>关闭</mu-list-item-title>
         <mu-divider/>
+        <mu-list-item button>
+          <mu-list-item-title>关闭</mu-list-item-title>
+        </mu-list-item>
       </mu-list>
     </mu-drawer>
   </div>
