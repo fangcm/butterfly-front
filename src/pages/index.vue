9b3480fd344006e8059311d69cf37a77f2d5f05b
fangcm <fangcm2008@qq.com>
2019-02-01 22:48:57 +0800

    o

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,23 +1,17 @@
 <template>
-  <div class="lanmuWrapper">
-    <mu-flex class="flex-wrapper" align-items="center" v-for="menu in menuList">
-      <mu-flex class="flex-wrapper" justify-content="center" fill>
-        <mu-sub-header> {{menu.name}}</mu-sub-header>
-      </mu-flex>
-      <mu-flex class="flex-child" justify-content="center" v-for="child in menu.children" :key="child.id" fill>
-        <router-link to="/MeiShi">
-          <span class="new-icon-circle iconfont dianying" style="background: #ff6767"></span>
-          <span class="icon-desc">{{child.name}}</span>
-        </router-link>
-      </mu-flex>
-    </mu-flex>
+  <div class="channel">
+    <ul class="channelList">
+      <li class="channelListItem" v-for="menu in menuList" :key="menu.id" @click="toChannelList(item, true)">
+        <img :src="menu.icon" width="100%"/>
+        <span class="title">{{menu.name}}</span>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
   import {userMenu} from '@/api/user-api'
   import {mapActions} from 'vuex'
-  import dataMenu from '@/api/data-menu'
 
   export default {
     name: "index",
@@ -45,53 +39,29 @@
 </script>
 
 <style scoped>
-  .lanmuWrapper {
-    background-color: #ffffff;
-    border-top: 1px solid #DDD8CE;
-    width: 100%;
-    height: auto;
-    margin-top: 4px;
+
+  .channelList {
+    position: absolute;
+    top: 56px;
+    left: 0;
+    right: 0;
+    bottom: 56px;
   }
 
-  .flex-wrapper {
-    width: 100%;
-    height: auto;
-  }
-
-  .flex-child {
-    width: 25%;
+  .channelList .channelListItem {
     display: inline-block;
-    box-sizing: border-box;
+    padding: 20px;
+    width: 25%;
     text-align: center;
   }
 
-  .flex-child:first-child {
-    margin-left: 0;
-  }
-
-  .flex-wrapper .flex-child > a {
-    display: block;
-    padding: 10px 0px;
+  .channelList .channelListItem img {
     width: 100%;
-    height: auto;
+    max-width: 50px;
   }
 
-  .flex-wrapper .flex-child .new-icon-circle {
+  .channelList .channelListItem span {
     display: block;
-    margin: auto;
-    margin-bottom: 7px;
-    width: 40px;
-    height: 40px;
-    border-radius: 500px;
-    text-align: center;
-    line-height: 40px;
-    font-size: 25px;
-    color: #fff;
-  }
-
-  .flex-wrapper .flex-child .icon-desc {
-    text-align: center;
-    font-size: 12px;
-    color: #666;
+    color: #333;
   }
 </style>
