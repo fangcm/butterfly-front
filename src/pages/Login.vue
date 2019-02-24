6d8da9920a8224c39d274f4dbff6d9bcc72606a9
fangcm <fangcm2008@qq.com>
2019-02-24 22:21:13 +0800

    ok

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,8 +1,8 @@
 <template>
   <v-app id="inspire">
     <v-content>
-      <v-container fluid fill-height>
-        <v-layout align-center justify-center>
+      <v-container fluid>
+        <v-layout justify-center>
           <v-flex xs12 sm8 md4>
             <v-card>
               <v-toolbar dark color="primary">
--- src/pages/common/Header.vue
+++ src/pages/common/Header.vue
@@ -1,40 +1,9 @@
 <template>
   <div>
-    <!-- 侧边栏 -->
-    <v-navigation-drawer v-model="drawer" fixed clipped app>
-      <v-list dense class="pa-1">
-        <v-list-tile avatar>
-          <v-list-tile-avatar>
-            <v-img class="elevation-6" :src="require('@/assets/image/head.jpg')"/>
-          </v-list-tile-avatar>
-          <v-list-tile-content>
-            <v-list-tile-title>John Leider</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-
-      <v-list dense class="pt-0">
-        <v-divider></v-divider>
-        <v-list-tile @click="toPage('/login')">
-          <v-list-tile-action>
-            <v-icon>settings</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>设置</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-        <v-list-tile v-if="isLogin" @click="logout()">
-          <v-list-tile-action>
-            <v-icon>exit_to_app</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>退出登录</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
     <v-toolbar dense fixed clipped-left app dark color="primary">
-      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-btn icon @click="routerBack()">
+        <v-icon>arrow_back</v-icon>
+      </v-btn>
       <v-btn icon @click="toPage('/app/index')">
         <v-icon>home</v-icon>
       </v-btn>
@@ -72,6 +41,20 @@
       logout() {
         this.loginOut();
         this.$router.push({path: '/login'})
+      },
+      routerBack() {
+        /*        if(this.$store.state.sessionToken){
+                  if(this.$store.state.isFirst){
+                    this.setIsFirst(false);
+                    this.$router.go(-1);
+                    this.$router.go(-1);
+                  }else{
+                    this.$router.go(-1)
+                  }
+                }else{
+                  this.$router.go(-1)
+                }*/
+        this.$router.go(-1)
       }
     },
     components: {},
